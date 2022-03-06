d96f03fab3034f3d822be9b560b76337d5200c1d
Simon Lorand <lorand.simon@acetech.dev>
2022-03-06 17:54:25 +0200

    optimization for mobile

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelectorAll('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  for (let i = 0; i < modal.length; i++) {
+    modal[i].classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
 };
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
+// const openModal = function () {
+//   for (let i = 0; i < modal.length; i++) {
+//     modal[i].classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+//   }
+// };
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', openModal);
+  btnOpenModal[i].addEventListener('click', function () {
+    modal[i].classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
 }
 
-btnCloseModal.addEventListener('click', closeModal);
+for (let i = 0; i < btnCloseModal.length; i++) {
+  btnCloseModal[i].addEventListener('click', closeModal);
+}
 
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', event => {
   console.log(event);
   //also checks if the modal is open
-  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
+  for (let i = 0; i < modal.length; i++) {
+    if (event.key === 'Escape' && !modal[i].classList.contains('hidden')) {
+      closeModal();
+    }
   }
 });
